c6ced24b7e05fc3c6db76bff256d16f341102e74
Kevin Nolan <kevnolan@gmail.com>
2014-12-31 20:48:47 +0000

    misc

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ Not every new computer comes with a Yeoman pre-installed. He lives in the [npm](
 npm install -g yo
 ```
 
+
 ### Yeoman Generators
 
 Yeoman travels light. He didn't pack any generators when he moved in. You can think of a generator like a plug-in. You get to choose what type of application you wish to create, such as a Backbone application or even a Chrome extension.
@@ -35,6 +36,7 @@ Finally, initiate the generator:
 yo allforabit
 ```
 
+
 ### Getting To Know Yeoman
 
 Yeoman has a heart of gold. He's a person with feelings and opinions, but he's very easy to work with. If you think he's too opinionated, he can be easily convinced.
@@ -42,6 +44,8 @@ Yeoman has a heart of gold. He's a person with feelings and opinions, but he's v
 If you'd like to get to know Yeoman better and meet some of his friends, [Grunt](http://gruntjs.com) and [Bower](http://bower.io), check out the complete [Getting Started Guide](https://github.com/yeoman/yeoman/wiki/Getting-Started).
 
 
+
 ## License
 
 MIT
+
--- app/index.js
+++ app/index.js
@@ -1,62 +1,116 @@
 'use strict';
+
+var join = require('path').join;
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
-var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
-  initializing: function () {
+  constructor: function () {
+    yeoman.generators.Base.apply(this, arguments);
+
     this.pkg = require('../package.json');
+
+    this.includeSass = true;
+    this.includeBootstrap = true;
+    this.includeModernizr = true;
+
+    this.includeLibSass = true;
+    this.includeRubySass = false;
   },
 
-  prompting: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the gnarly' + chalk.red('Allforabit') + ' generator!'
-    ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
-
-      done();
-    }.bind(this));
+  gruntfile: function () {
+    this.copy('Gruntfile.js');
   },
 
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
-    },
+  packageJSON: function () {
+    this.template('_package.json', 'package.json');
+  },
 
-    projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
+  git: function () {
+    this.template('gitignore', '.gitignore');
+    this.copy('gitattributes', '.gitattributes');
+  },
+
+  bower: function () {
+
+    var bower = {
+      name: this._.slugify(this.appname),
+      private: true,
+      dependencies: {}
+    };
+
+    var bs = 'bootstrap' + (this.includeSass ? '-sass-official' : '');
+    bower.dependencies[bs] = "~3.2.0";
+
+    bower.dependencies.modernizr = '~2.8.2';
+    bower.dependencies.marionette = '~2.3.0';
+    bower.dependencies['backbone.select'] = '~1.2.8';
+
+    this.copy('bowerrc', '.bowerrc');
+    this.write('bower.json', JSON.stringify(bower, null, 2));
+  },
+
+  jshint: function () {
+    this.copy('jshintrc', '.jshintrc');
+  },
+
+  editorConfig: function () {
+    this.copy('editorconfig', '.editorconfig');
+  },
+
+  mainStylesheet: function () {
+    this.template('main.scss', 'app/styles/main.scss');
+  },
+
+  writeIndex: function () {
+    this.indexFile = this.engine(
+      this.readFileAsString(join(this.sourceRoot(), 'index.html')),
+      this
+    );
+
+    // wire Bootstrap plugins
+    if (this.includeBootstrap && !this.includeSass) {
+      var bs = 'bower_components/bootstrap/js/';
+
+      this.indexFile = this.appendFiles({
+        html: this.indexFile,
+        fileType: 'js',
+        optimizedPath: 'scripts/plugins.js',
+        sourceFileList: [
+          bs + 'affix.js',
+          bs + 'alert.js',
+          bs + 'dropdown.js',
+          bs + 'tooltip.js',
+          bs + 'modal.js',
+          bs + 'transition.js',
+          bs + 'button.js',
+          bs + 'popover.js',
+          bs + 'carousel.js',
+          bs + 'scrollspy.js',
+          bs + 'collapse.js',
+          bs + 'tab.js'
+        ],
+        searchPath: '.'
+      });
     }
+
+    this.indexFile = this.appendFiles({
+      html: this.indexFile,
+      fileType: 'js',
+      optimizedPath: 'scripts/main.js',
+      sourceFileList: ['scripts/main.js'],
+      searchPath: ['app', '.tmp']
+    });
   },
 
-  install: function () {
-    this.installDependencies({
-      skipInstall: this.options['skip-install']
-    });
+  app: function () {
+    this.directory('app');
+    this.mkdir('app/scripts');
+    this.mkdir('app/styles');
+    this.mkdir('app/images');
+    this.write('app/index.html', this.indexFile);
+
+    this.copy('main.js', 'app/scripts/main.js');
   }
+
 });
